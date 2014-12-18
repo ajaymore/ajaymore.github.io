ba85c8ff57c9464969dbec87fbbbe2b09dabac18
unknown <ajmore.biz@gmail.com>
2014-12-18 23:47:53 +0530

    App upgrade.

--- notes/regular/assets/app.js
+++ notes/regular/assets/app.js
@@ -1,151 +1,70 @@
 var showHide = function ($scope, showArr, hideArr) {
-    for (var i = 0; i < showArr.length; i++) {
-        $scope[showArr[i]] = true;
-    }
     for (var i = 0; i < hideArr.length; i++) {
         $scope[hideArr[i]] = false;
     }
-};
-var showLoading = function ($scope, show) {
-    $scope.$parent.showLoading = show;
-}
-var createLinks = function ($scope) {
-    showLoading($scope, false);
-    $("li").each(function (index) {
-        var url = $(this).text();
-        if (url.indexOf('http://') === 0 || url.indexOf('https://') === 0) {
-            var liElem = $('<li />')
-            var anchorElem = $('<a />', {text: url, href: url, target: '_blank'})
-            liElem.append(anchorElem);
-            $(this).replaceWith(liElem);
-        }
-    });
-};
-function smoothScroll(el, to, duration) {
-    if (duration < 0) {
-        return;
+    for (var i = 0; i < showArr.length; i++) {
+        $scope[showArr[i]] = true;
     }
-    var difference = to - $(window).scrollTop();
-    var perTick = difference / duration * 10;
-    this.scrollToTimerCache = setTimeout(function () {
-        if (!isNaN(parseInt(perTick, 10))) {
-            window.scrollTo(0, $(window).scrollTop() + perTick);
-            smoothScroll(el, to, duration - 10);
-        }
-    }.bind(this), 10);
-}
-$(document).ready(function () {
-    $('body').doubleTap(function () {
-        smoothScroll($(window), 0, 300);
-    });
-    $('body').dblclick(function () {
-        smoothScroll($(window), 0, 300);
-    });
-});
+};
+angular.module('myApp', ['ngMaterial'])
+    .directive("setCss", function () {
+        return {
+            restrict: "A",
+            link: function (scope, element) {
+                element.css({
+                    'overflow': 'auto'
+                });
+            }
+        };
+    })
+    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $http) {
+        var jsonLocation = 'assets';
+        $scope.toggleLeft = function () {
+            $mdSidenav('left').toggle();
+        };
+        $scope.close = function () {
+            $mdSidenav('left').close();
+        };
+        $scope.sideNavToolBarText = "Subjects";
+        $scope.showSubjects = true;
+        $scope.subChaptList = [];
+        $scope.subjects = [];
+        $scope.titleText = "How to use notes application";
+        $scope.template = {
+            url: 'tools/how to.html'
+        };
 
-var jsonLocation = 'assets';
-var myApp = angular.module('myApp', ['ngAnimate']);
-myApp.controller('MobileCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
-        showLoading($scope, false);
-        $scope.template = {};
-        var titleArr = ['Subjects'];
-        $scope.title = titleArr[titleArr.length - 1];
         $http.get(jsonLocation + '/subjects.json').success(function (data, status, headers, config) {
+            $scope.subChaptList = data;
             $scope.subjects = data;
         }).error(function (data, status, headers, config) {
             console.log('subjects fetch error');
         });
-        showHide($scope, ['showSubjects'], ['showBackBtn', 'showChapters', 'showNote']);
-        var level = 0;
-        var currentSubject;
-        $scope.subjectClicked = function (subject) {
-            showLoading($scope, true);
-            currentSubject = subject;
-            $http.get(jsonLocation + '/' + subject + '.json').success(function (data, status, headers, config) {
-                showLoading($scope, false);
-                $scope.chapters = data;
-                showHide($scope, ['showChapters', 'showBackBtn'], ['showSubjects']);
-                level++;
-                titleArr.push(subject);
-                $scope.title = titleArr[titleArr.length - 1];
-            }).error(function (data, status, headers, config) {
-                console.log('chapters fetch error');
-            });
-        };
-
-        var currentChapter;
-        $scope.chapterClicked = function (chapter) {
-            if (currentChapter !== chapter) {
-                showLoading($scope, true);
-            }
-            currentChapter = chapter;
-            $scope.template.url = currentSubject + '/' + chapter + '.html';
-            level++;
-            titleArr.push(chapter);
-            $scope.title = titleArr[titleArr.length - 1];
-            showHide($scope, ['showNote'], ['showSubjects', 'showChapters']);
-        };
-        $scope.createLinks = function () {
-            createLinks($scope);
-        };
-
-        $scope.backClicked = function () {
-            level--;
-            titleArr.pop();
-            $scope.title = titleArr[titleArr.length - 1];
-            switch (level) {
-                case 0:
-                    showHide($scope, ['showSubjects'], ['showNote', 'showChapters', 'showBackBtn']);
-                    break;
-                case 1:
-                    showHide($scope, ['showChapters', 'showBackBtn'], ['showSubjects', 'showNote']);
-                    break;
-            }
-        };
-    }]).controller('DesktopCtrl', ['$scope', '$location', '$http', function ($scope, $location, $http) {
-        showLoading($scope, false);
-        $scope.template = {url: 'tools/how to.html'};
-        var titleArr = ['Subjects'];
-        $scope.title = titleArr[titleArr.length - 1];
-        $http.get(jsonLocation + '/subjects.json').success(function (data, status, headers, config) {
-            $scope.subjects = data;
-        }).error(function (data, status, headers, config) {
-            console.log('subjects fetch error');
-        });
-        showHide($scope, ['showSubjects', 'showNote'], ['showBackBtn']);
 
         var currentSubject;
-        $scope.subjectClicked = function (subject) {
-            currentSubject = subject;
-            showLoading($scope, true);
-            $http.get(jsonLocation + '/' + subject + '.json').success(function (data, status, headers, config) {
-                showLoading($scope, false);
-                $scope.chapters = data;
-                showHide($scope, ['showChapters', 'showBackBtn', 'showNote'], ['showSubjects']);
-                titleArr.push(subject);
-                $scope.title = titleArr[titleArr.length - 1];
-            }).error(function (data, status, headers, config) {
-                console.log('chapters fetch error');
-            });
-        }
-
-        var currentChapter;
-        $scope.chapterClicked = function (chapter) {
-            if (currentChapter !== chapter) {
-                showLoading($scope, true);
+        $scope.itemClicked = function (item) {
+            $scope.titleText = item;
+            if (!currentSubject) {
+                currentSubject = item;
+                $http.get(jsonLocation + '/' + item + '.json').success(function (data, status, headers, config) {
+                    $scope.subChaptList = data;
+                    showHide($scope, ['showBackBtn'], []);
+                }).error(function (data, status, headers, config) {
+                    console.log('chapters fetch error');
+                });
+            } else {
+                $scope.template.url = currentSubject + '/' + item + '.html';
+                if ($mdSidenav('left').isOpen()) {
+                    $mdSidenav('left').close();
+                }
             }
-            currentChapter = chapter;
-            $scope.template.url = currentSubject + '/' + chapter + '.html';
-            titleArr.push(chapter);
-            $scope.title = titleArr[titleArr.length - 1];
-        }
-        $scope.createLinks = function () {
-            createLinks($scope);
         };
 
-        $scope.backClicked = function () {
-            titleArr.splice(1, 2);
-            $scope.title = titleArr[titleArr.length - 1];
-            showHide($scope, ['showSubjects'], ['showChapters', 'showBackBtn']);
-        };
-    }]);
+        $scope.backBtnPress = function () {
+            console.log('back pressed');
+            showHide($scope, [], ['showBackBtn']);
+            currentSubject = undefined;
+            $scope.subChaptList = $scope.subjects;
+        }
+
+    });
--- notes/regular/generate.php
+++ notes/regular/generate.php
@@ -8,14 +8,19 @@ function createJsonFile($fileName, $array){
 function removeExtension($str){
 	return str_replace('.md','',$str);
 }
+function test_html($str)
+{
+    return !strpos($str,'.html');
+}
 $dirs = array_filter(glob('*'), 'is_dir');
 $dirs = array_values($dirs);
-$dirs = array_diff($dirs, array('_site', 'assets', 'sass'));
+$dirs = array_diff($dirs, array('_site', 'assets', 'sass', 'node_modules', 'nbproject', 'build'));
 createJsonFile("subjects", $dirs);
 foreach ($dirs as $dir)
 {
 	$files = scandir($dir);
 	$files = array_diff($files, array('.', '..'));
 	$files = array_map('removeExtension', $files);
+    $files = array_filter($files,"test_html");
 	createJsonFile($dir, $files);
 }
